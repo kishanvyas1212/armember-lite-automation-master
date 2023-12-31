55e08c177eeaf94e21382c7e524766290e0e2f40
Kishan Vyas <kishanvyas12kv@gmail.com>
2023-12-31 08:03:18 +0530

    new changes

--- features/login.py
+++ features/login.py
@@ -13,9 +13,18 @@ ser_Obj = Service("C:\\Users\\91635\\Desktop\\drivers\\chromedriver.exe")
 # ser_Obj = Service("C:\\Users\\91635\\Desktop\\drivers\\geckodriver.exe")
 drivers = webdriver.Chrome(service= ser_Obj)
 drivers.implicitly_wait(60)
-
-def wait_for_element(driver, locator, timeout=10):
-    return WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
+url = "http://localhost/test_kv1/"
+def wait_for_element_present(driver, locator, timeout=10):
+    try: 
+        # try to find the element is located or not, if not then return None
+        WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
+            
+    except:
+        pass
+    return 
+def wait_for_element_display(driver, locator, timeout=10):
+    return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
+# locator = (By.XPATH,"test") pass the locator in this way for wait for element 
 
 def action_perform(element, action,value):
     if action=='click':
@@ -26,41 +35,47 @@ def action_perform(element, action,value):
     
     
 def findelement(driver, locator,locatorpath,action=1,value=1):
-    if locator=="ID" or locator=="id":
-        element = driver.find_element(By.ID, locatorpath )
-        if action!="" or action!="1" or action=="click" or action=="send_keys":
-            action_perform(element,action,value)
-        return element
-    elif locator=="XPATH" or locator=="xpath":
-        element = driver.find_element(By.XPATH,locatorpath)
-        if action!="" or action!="1" or action=="click" or action=="send_keys":
-            action_perform(element,action,value)
-        return element
-    elif locator=="name" or locator=="NAME":
-        element = driver.find_element(By.NAME,locatorpath)
-        if action!="" or action!="1" or action=="click" or action=="send_keys":    
-            action_perform(element,action,value)
-        return element
+    try:
+        if locator=="ID" or locator=="id":
+            element = driver.find_element(By.ID, locatorpath )
+            if action!="" or action!="1" or action=="click" or action=="send_keys":
+                action_perform(element,action,value)
+            return element
+        elif locator=="XPATH" or locator=="xpath":
+            element = driver.find_element(By.XPATH,locatorpath)
+            if action!="" or action!="1" or action=="click" or action=="send_keys":
+                action_perform(element,action,value)
+            return element
+        elif locator=="name" or locator=="NAME":
+            element = driver.find_element(By.NAME,locatorpath)
+            if action!="" or action!="1" or action=="click" or action=="send_keys":    
+                action_perform(element,action,value)
+            return element
+    except Exception as e:
+        print("Element is not found so please retry to move to next case")
+        return 1        
 
 
-url = "http://localhost/test_kv1/"
-end_point= "wp-login.php"
-username = "admin"
-password = "adminconnect"
-drivers.get(url + end_point)
-drivers.implicitly_wait(10)
-usernamef = findelement(drivers,"ID","user_login","send_keys",username)
-pwd = findelement(drivers,"ID","user_pass","send_keys",password)
-beforelogin = drivers.get_cookies()
-login = findelement(drivers,"NAME","wp-submit","click")
-armember=findelement(drivers,"XPATH",'//*[@id="toplevel_page_arm_manage_members"]/a/div[3]',"")
-print(armember)
-afterlogin = drivers.get_cookies()
-print("cookies before login")
-print(beforelogin)
-print("after login cookies")
-print(afterlogin)
-armember.click()
-time.sleep(10)
+def login_with_wordpress():
+    end_point= "wp-login.php"
+    username = "admin"
+    password = "adminconnect"
+    drivers.get(url + end_point)
+    usernamef = findelement(drivers,"ID","user_login","send_keys",username)
+    pwd = findelement(drivers,"ID","user_pass","send_keys",password)
+    beforelogin = drivers.get_cookies()
+    login = findelement(drivers,"NAME","wp-submit","click")
+    armember=findelement(drivers,"XPATH",'//*[@id="toplevel_page_arm_manage_members"]/a/div[3]',"")
+    print(armember)
+    afterlogin = drivers.get_cookies()
+    print("cookies before login")
+    print(beforelogin)
+    print("after login cookies")
+    print(afterlogin)
+    armember.click()
+    time.sleep(10)
 
+
+
+login = login_with_wordpress()
 drivers.quit()
